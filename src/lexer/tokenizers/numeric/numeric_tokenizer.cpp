dfea20588577bab6421a1d0fc033cc260aafc634
Maksimilijan Marošević <mm51593@fer.hr>
2024-04-13 20:40:05 +0200

    Add numeric tokenizer

---
+++ src/lexer/tokenizers/numeric/numeric_tokenizer.cpp
@@ -0,0 +1,117 @@
+#include "numeric_tokenizer.hpp"
+
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <variant>
+
+#define RADIX 10
+
+using Initial = NumericTokenizer::Initial;
+using Whole = NumericTokenizer::Whole;
+using Decimal = NumericTokenizer::Decimal;
+using Invalid = NumericTokenizer::Invalid;
+
+//////////////////////////////////////////
+
+struct Digit {
+  uint32_t x;
+};
+struct DecimalPoint {};
+
+using Input = std::variant<Digit, DecimalPoint, std::monostate>;
+
+Input from(uint8_t raw_input) {
+  if (raw_input == '.') {
+    return DecimalPoint{};
+  } else if (isdigit(raw_input)) {
+    uint32_t value = raw_input - '0';
+    return Digit{.x = value};
+  } else {
+    return std::monostate();
+  }
+}
+
+//////////////////////////////////////////
+
+NumericTokenizer::State transition(Initial state, Digit input) {
+  return Whole{.x = input.x};
+}
+NumericTokenizer::State transition(Initial state, DecimalPoint input) {
+  return Decimal{};
+}
+NumericTokenizer::State transition(Whole state, Digit input) {
+  state.x = state.x * RADIX + input.x;
+  return state;
+}
+NumericTokenizer::State transition(Whole state, DecimalPoint input) {
+  return Decimal{};
+}
+NumericTokenizer::State transition(Decimal state, Digit input) {
+  state.x = state.x * RADIX + input.x;
+  return state;
+}
+NumericTokenizer::State transition(auto state, auto input) { return Invalid{}; }
+
+//////////////////////////////////////////
+
+template <> void NumericTokenizer::update(Whole state) {
+  this->whole = state.x;
+}
+template <> void NumericTokenizer::update(Decimal state) {
+  this->decimal = state.x;
+}
+void NumericTokenizer::update(auto state) {}
+
+//////////////////////////////////////////
+
+NumericTokenizer::Result get_result(NumericTokenizer *tknzr, Initial old_state,
+                                    Invalid new_state) {
+  return NumericTokenizer::Error{};
+}
+NumericTokenizer::Result get_result(NumericTokenizer *tknzr, Invalid old_state,
+                                    Invalid new_state) {
+  return NumericTokenizer::Error{};
+}
+NumericTokenizer::Result get_result(NumericTokenizer *tknzr, auto old_state,
+                                    Invalid new_state) {
+  return NumericTokenizer::Done{tknzr->produce_result()};
+}
+NumericTokenizer::Result get_result(NumericTokenizer *tknzr, auto old_state,
+                                    auto new_state) {
+  return NumericTokenizer::Pending{};
+}
+
+//////////////////////////////////////////
+
+Token NumericTokenizer::produce_result() {
+  uint64_t base = RADIX;
+
+  while (this->decimal > base) {
+    base *= RADIX;
+  }
+
+  return NumericToken{.value = this->whole + (double)this->decimal / base};
+}
+
+NumericTokenizer::Result NumericTokenizer::step(uint8_t raw_input) {
+  Input input = from(raw_input);
+
+  auto next_state = std::visit(
+      [this](auto next_state, auto input) {
+        auto temp_state = transition(next_state, input);
+        std::visit([this](auto next_state) { this->update(next_state); },
+                   temp_state);
+        return temp_state;
+      },
+      this->state, input);
+
+  auto res = std::visit(
+      [this, next_state](auto old_state, auto new_state) {
+        return get_result(this, old_state, new_state);
+      },
+      this->state, next_state);
+
+  this->state = next_state;
+  return res;
+}
---
+++ src/lexer/tokenizers/numeric/numeric_tokenizer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../tokenizer.hpp"
+
+#include <cstdint>
+#include <variant>
+
+class NumericTokenizer : public Tokenizer {
+public:
+    struct Initial {};
+    struct Whole { uint64_t x = 0; };
+    struct Decimal { uint64_t x = 0; };
+    struct Invalid {};
+
+    using State = std::variant<Initial, Whole, Decimal, Invalid>; 
+
+    State state = Initial{};
+
+    Result step(uint8_t raw_input) override;       
+
+    void update(auto state);
+
+    Token produce_result();
+
+private:
+    uint64_t whole = 0;
+    uint64_t decimal = 0;
+};
