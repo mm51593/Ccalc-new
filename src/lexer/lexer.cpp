99fe0cdbdad08e5a9b55ecb68a46a851aacc76cb
Maksimilijan Marosevic <m.marosevic@reply.de>
2024-05-27 14:54:11 +0200

    Integrate lexer with reader

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -1,4 +1,5 @@
 #include "lexer.hpp"
+#include "src/lexer/token.hpp"
 #include "tokenizers/tokenizer.hpp"
 
 #include <cstdint>
@@ -6,12 +7,11 @@
 #include <variant>
 #include <vector>
 
+#define INVALID_CHAR 0
+
 const Lexer::Tokenizers Lexer::tokenizers_template = {
-    WhitespaceTokenizer{},
-    NumericTokenizer{},
-    OperatorTokenizer{},
-    IdentifierTokenizer{},
-    KeywordTokenizer{},
+    WhitespaceTokenizer{}, NumericTokenizer{}, OperatorTokenizer{},
+    IdentifierTokenizer{}, KeywordTokenizer{},
 };
 
 //////////////////////////////////////////
@@ -26,39 +26,53 @@ Tokenizer::Result feed_char(T &tokenizer, uint8_t raw_input) {
 
 //////////////////////////////////////////
 
-Token Lexer::get_next(std::string::iterator &it) {
+Lexer::Lexer(Reader &rd) : rd(rd) {}
+
+bool Lexer::sort_results(std::vector<Token> &products,
+                         std::array<Result, tknzr_size> &results) {
+  bool ret = false;
+  for (auto res : results) {
+    if (Tokenizer::Pending *real_res = std::get_if<Tokenizer::Pending>(&res)) {
+      ret = true;
+    } else if (Tokenizer::Done *real_res = std::get_if<Tokenizer::Done>(&res)) {
+      products.push_back(real_res->token);
+    }
+  }
+  return ret;
+}
+
+void Lexer::feed_tokenizers(uint8_t c, std::array<Result, tknzr_size> &results,
+                            uint8_t &results_index, Tokenizers &tokenizers) {
+
+  std::apply(
+      [c, &results, &results_index](auto &...args) {
+        ((results[results_index++] = feed_char(args, c)), ...);
+      },
+      tokenizers);
+}
+
+void Lexer::tokenizer_loop(Tokenizers &tokenizers,
+                           std::vector<Token> &products) {
+  std::array<Result, tknzr_size> results;
+  uint8_t results_index;
+  bool should_loop;
+
+  do {
+    uint8_t c = (*(this->rd)).value_or(INVALID_CHAR);
+    results_index = 0;
+
+    feed_tokenizers(c, results, results_index, tokenizers);
+    should_loop = sort_results(products, results);
+  } while (should_loop);
+}
+
+Token Lexer::next() {
   Tokenizers tokenizers = this->tokenizers_template;
 
   std::vector<Token> products;
   products.reserve(tknzr_size);
 
-  std::array<Result, tknzr_size> results;
-  uint8_t results_index;
-
-  bool shouldLoop;
-  do {
-    shouldLoop = false;
-    results_index = 0;
-    std::apply(
-        [it, &results, &results_index](auto &...args) {
-          ((results[results_index++] = feed_char(args, *it)), ...);
-        },
-        tokenizers);
-
-    for (auto res : results) {
-      if (Tokenizer::Pending *real_res =
-              std::get_if<Tokenizer::Pending>(&res)) {
-        shouldLoop = true;
-      } else if (Tokenizer::Done *real_res =
-                     std::get_if<Tokenizer::Done>(&res)) {
-        products.push_back(real_res->token);
-      }
-    }
-
-    if (shouldLoop) {
-      it++;
-    }
-  } while (shouldLoop);
+  tokenizer_loop(tokenizers, products);
 
   return products.back();
 }
--- src/lexer/lexer.hpp
+++ src/lexer/lexer.hpp
@@ -6,6 +6,9 @@
 #include "tokenizers/operator/operator_tokenizer.hpp"
 #include "tokenizers/tokenizer.hpp"
 
+#include "src/reader/reader.hpp"
+
+#include <array>
 #include <cstdint>
 #include <tuple>
 
@@ -14,10 +17,17 @@ public:
   using Tokenizers = std::tuple<WhitespaceTokenizer, NumericTokenizer, OperatorTokenizer,
                                 IdentifierTokenizer, KeywordTokenizer>;
   constexpr static uint8_t tknzr_size = std::tuple_size<Tokenizers>();
+
   using Result = Tokenizer::Result;
 
-  Token get_next(std::string::iterator &it);
+  Lexer(Reader &rd);
+  Token next();
 
 private:
   const static Tokenizers tokenizers_template;
+  void tokenizer_loop(Tokenizers &tokeniers, std::vector<Token> &products);
+  bool sort_results(std::vector<Token> &products, std::array<Result, tknzr_size> &results);
+  void feed_tokenizers(uint8_t c, std::array<Result, tknzr_size> &results, uint8_t &results_index, Tokenizers &tokenizers);
+
+  Reader &rd;
 };
